b16e3996e9a026026de1dfc349c2f5540831f407
Eason <trueluo1987@gmail.com>
2016-05-20 17:29:38 +1000

    create outline file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>Laravel</title>
-
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
         <style>
@@ -35,11 +35,32 @@
             }
         </style>
     </head>
-    <body>
+    <body ng-app="todo" ng-controller="todocontroller">
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
             </div>
         </div>
+        
+        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.js"></script>
+		<script>
+			(function($){
+				$(document).ready(function(){
+					var app = angular.module('app',[]);
+					app.controller('todocontroller', function($scope, $http, $config){
+						
+					});
+
+					app.directive('el:todo', function(){
+						return {
+							templateUrl:'templates/todo.html',
+						};
+					});
+				});
+			})(jQuery);
+		</script>
     </body>
 </html>
